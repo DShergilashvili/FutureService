805eaa29f906dbd3d23361734aa284bcb52e8792
dshergilashvili <davitshergilashvili@gmail.com>
2021-10-28 01:38:21 +0400

    xzy3

---
+++ FutureService.Api/Features/Todo/TodoModule.cs
@@ -0,0 +1,20 @@
+﻿namespace FutureService.Api.Features.Todo
+{
+    public class TodoModule : IModule
+    {
+        private IMediator _mediator;
+        private IMapper _mapper;
+        public IEndpointRouteBuilder MapEndpoints(IEndpointRouteBuilder endpoints)
+        {
+            return endpoints;
+        }
+
+        public WebApplicationBuilder RegisterModule(WebApplicationBuilder builder)
+        {
+            var provider = builder.Services.BuildServiceProvider();
+            _mediator = provider.GetRequiredService<IMediator>();
+            _mapper = provider.GetRequiredService<IMapper>();
+            return builder;
+        }
+    }
+}
